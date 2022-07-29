f5ac5b07d5ca18a9e2a4c41a2166881f34076976
itswakana <wakanawardrobe@gmail.com>
2022-07-29 15:31:59 +0100

    add export module for other pages

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import {pageLoad} from './load';
+import {mainPage} from './home';
+import {menuPage} from './menu';
 
-pageLoad();
+window.onload = mainPage();
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        clearPage();
+        menuPage();
+    });
+});
+
+function clearPage() {
+    
+}
---
+++ src/menu.js
@@ -0,0 +1,8 @@
+export function menuPage() {
+    const content = document.getElementById('content');
+
+    const title = document.createElement('h3');
+    title.textContent = 'Recipes';
+
+    content.append(title);
+}
