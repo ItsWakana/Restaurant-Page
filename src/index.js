e3672cbdf94d3ba886bc8baae4862839551b1ab4
itswakana <wakanawardrobe@gmail.com>
2022-07-29 16:03:18 +0100

    add function for clearing previous page

--- src/index.js
+++ src/index.js
@@ -8,10 +8,18 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         clearPage();
-        menuPage();
+        // menuPage();
     });
 });
 
 function clearPage() {
-    
+    const content = document.getElementById('content');
+    const imgs = document.querySelectorAll('img');
+
+    const headings = document.querySelectorAll('h3');
+    const paras = document.querySelectorAll('p');
+
+    const elementsToRemove = [...imgs, ...headings, ...paras];
+
+    elementsToRemove.forEach(element => element.remove());
 }
